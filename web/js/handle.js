a23cdfc8a36fa6771cc5254b7e4eaa5a2e267e45
Brx86 <brx86@qq.com>
2023-04-07 11:55:25 +0800

    add ;

--- web/js/handle.js
+++ web/js/handle.js
@@ -2,7 +2,7 @@ const langSelect = document.getElementById("lang-select");
 const textInput = document.getElementById("text-input");
 const textOutput = document.getElementById("text-output");
 const customAlert = document.getElementById("custom-alert");
-const api_base = `${window.location.protocol}//${window.location.hostname}/`
+const api_base = `${window.location.protocol}//${window.location.hostname}/`;
 
 function myAlert(message) {
     customAlert.textContent = message;
@@ -10,22 +10,24 @@ function myAlert(message) {
     setTimeout(function () {
         customAlert.style.top = "-100px";
     }, 2000);
-}
+};
 async function btnTranslate() {
-    let api_url = api_base + 'api'
+    let api_url = api_base + 'api';
+    let text_loading = "加载中...";
     let lang_select = langSelect.value;
     let text_input = textInput.value.trim();
     if (lang_select === "") {
-        myAlert("请选择语言！")
+        myAlert("请选择语言！");
         return;
     } else if (lang_select === "ZHGPT") {
-        api_url = api_base + 'gpt'
-    }
+        api_url = api_base + 'gpt';
+        text_loading += 'chatgpt可能不稳定，请耐心等待';
+    };
     if (text_input === "") {
-        myAlert("请输入文本！")
+        myAlert("请输入文本！");
         return;
-    }
-    customAlert.textContent = "加载中...";
+    };
+    customAlert.textContent = text_loading;
     customAlert.style.top = "30px";
     let headers = { 'Content-Type': 'application/json' };
     let body_json = JSON.stringify({ text: text_input, target_lang: lang_select });
@@ -33,21 +35,21 @@ async function btnTranslate() {
     let r = await response.json();
     customAlert.style.top = "-100px";
     textOutput.value = r.data;
-}
+};
 async function btnCopy() {
     if (textOutput.value === "") {
-        myAlert("内容为空！")
+        myAlert("内容为空！");
         return;
-    }
+    };
     try {
         await navigator.clipboard.writeText(textOutput.value);
         myAlert('复制成功');
     } catch (error) {
         myAlert('复制失败');
-    }
-}
+    };
+};
 function btnClear() {
-    textInput.value = ""
-    textOutput.value = ""
-    myAlert("已清除！")
+    textInput.value = "";
+    textOutput.value = "";
+    myAlert("已清除！");
 }
